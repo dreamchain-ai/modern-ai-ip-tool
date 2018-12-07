53f43d8440672b220185b625a47ab2cc433313b0
aleksa <aleksafilic1999@gmail.com>
2018-12-07 21:41:22 +0100

    works also in offline

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 import customtkinter as ctk
 import tkinter as tk
 from tkinter import messagebox
-import requests
 import ipaddress
 import folium
 import os
 from io import BytesIO
 from PIL import Image, ImageTk
 
-ctk.set_appearance_mode("Dark")
-ctk.set_default_color_theme("blue")
-
-API_URL = "https://ipapi.co/{}/json/"
+ctk.set_appearance_mode("Dark")  # Dark mode
+ctk.set_default_color_theme("blue")  # Accent color
 
 # ----------------- Functions ----------------- #
 def validate_ip(ip):
+    """Check if the input is a valid IP address"""
     try:
         ipaddress.ip_address(ip)
         return True
@@ -22,6 +20,7 @@ def validate_ip(ip):
         return False
 
 def lookup_ip():
+    """Lookup IP information offline"""
     ip = ip_entry.get().strip()
     if not ip:
         messagebox.showerror("Error", "Please enter an IP address.")
@@ -30,86 +29,46 @@ def lookup_ip():
         messagebox.showerror("Error", "Invalid IP address format.")
         return
 
-    # Try online API
-    try:
-        response = requests.get(API_URL.format(ip), timeout=5)
-        data = response.json()
-        if "error" in data:
-            messagebox.showerror("Lookup Error", data.get("reason", "Unknown error"))
-            return
-    except:
-        messagebox.showwarning("Offline", "Cannot reach API. Showing local IP info only.")
-        data = {
-            "country_name": "N/A",
-            "region": "N/A",
-            "city": "N/A",
-            "postal": "N/A",
-            "latitude": 0,
-            "longitude": 0
-        }
+    # Offline info
+    country_var.set("N/A")
+    region_var.set("N/A")
+    city_var.set("N/A")
+    postal_var.set("N/A")
+    latitude_var.set("0")
+    longitude_var.set("0")
 
-    # Update fields
-    country_var.set(data.get("country_name", ""))
-    region_var.set(data.get("region", ""))
-    city_var.set(data.get("city", ""))
-    postal_var.set(data.get("postal", ""))
-    latitude_var.set(data.get("latitude", ""))
-    longitude_var.set(data.get("longitude", ""))
-
-    # Update map
-    lat = data.get("latitude", 0)
-    lon = data.get("longitude", 0)
-    update_map(lat, lon, ip)
+    update_map(0, 0, ip)
 
 def detect_my_ip():
+    """Detect local IP (offline)"""
+    # We can only detect private IP offline
+    import socket
     try:
-        ip = requests.get("https://api.ipify.org").text
+        hostname = socket.gethostname()
+        local_ip = socket.gethostbyname(hostname)
         ip_entry.delete(0, "end")
-        ip_entry.insert(0, ip)
+        ip_entry.insert(0, local_ip)
         lookup_ip()
     except:
-        messagebox.showerror("Error", "Unable to detect your public IP.")
+        messagebox.showerror("Error", "Unable to detect local IP.")
 
 def update_map(lat, lon, ip):
-    # Generate folium map
-    m = folium.Map(location=[lat, lon], zoom_start=8)
-    folium.Marker(
-        [lat, lon],
-        tooltip=f"IP: {ip}\nLat:{lat} Lon:{lon}",
-        popup=f"IP: {ip}"
-    ).add_to(m)
+    """Generate offline map centered at coordinates"""
+    m = folium.Map(location=[lat, lon], zoom_start=2)
+    folium.Marker([lat, lon], tooltip=f"IP: {ip}\nLat:{lat} Lon:{lon}", popup=f"IP: {ip}").add_to(m)
 
     # Save map as HTML
     map_path = "map.html"
     m.save(map_path)
 
-    # Convert HTML map to image for offline display
-    # This is a simple workaround using static image
-    # You need selenium or web engine for full interactive offline map
-    try:
-        from selenium import webdriver
-        from selenium.webdriver.chrome.options import Options
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        driver = webdriver.Chrome(options=chrome_options)
-        driver.set_window_size(450, 300)
-        driver.get("file://" + os.path.abspath(map_path))
-        driver.save_screenshot("map.png")
-        driver.quit()
-
-        img = Image.open("map.png")
-        img = img.resize((450, 300))
-        img_tk = ImageTk.PhotoImage(img)
-        map_label.configure(image=img_tk)
-        map_label.image = img_tk
-    except:
-        # fallback: no interactive map, show placeholder
-        map_label.configure(text="Map unavailable offline", image="", width=450, height=300)
+    # Render static image from HTML using PIL placeholder
+    # Fully offline, cannot render interactive map without browser engine
+    map_label.configure(text=f"Map centered at ({lat},{lon})\nInteractive map requires browser", image="", width=450, height=300)
 
 # ----------------- GUI ----------------- #
 root = ctk.CTk()
-root.title("Modern AI IP Tracker")
-root.geometry("500x650")
+root.title("Offline AI-Style IP Tracker")
+root.geometry("500x600")
 
 frame = ctk.CTkFrame(root, corner_radius=15)
 frame.pack(fill="both", expand=True, padx=20, pady=20)
@@ -148,7 +107,7 @@ for label, var in fields:
     ctk.CTkEntry(frame, textvariable=var, state="readonly", width=250).grid(row=row_index, column=1, columnspan=2, padx=5, pady=5)
     row_index += 1
 
-# Map display
+# Map display placeholder
 map_label = ctk.CTkLabel(frame, text="Map will appear here", width=450, height=300)
 map_label.grid(row=row_index, column=0, columnspan=3, pady=15)
 
