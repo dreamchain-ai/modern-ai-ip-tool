0a4fef545090f0e8e2db3187c9c8e6ee3d9d4ccb
aleksa <aleksafilic1999@gmail.com>
2023-11-01 20:44:41 +0100

    make big update

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import threading
 import time
 import os
 from multiprocessing import Process
+import requests
 
 # ---------------- CONFIG ---------------- #
 DATABASE_PATH = "GeoLite2-City.mmdb"
@@ -25,11 +26,13 @@ def validate_ip(ip):
         return False
 
 def animate_field_update(field_var, value):
+    """Animate text typing in entry field"""
     field_var.set("")
-    for i in range(3):
-        field_var.set(value[:i])
-        time.sleep(0.05)
-    field_var.set(value)
+    def update_char(i=0):
+        if i <= len(value):
+            field_var.set(value[:i])
+            root.after(50, update_char, i+1)
+    update_char()
 
 def generate_map(lat, lon, ip):
     """Generate Folium map HTML"""
@@ -37,6 +40,22 @@ def generate_map(lat, lon, ip):
     folium.Marker([lat, lon], tooltip=f"IP: {ip}", popup=f"IP: {ip}").add_to(m)
     m.save(MAP_FILE)
 
+# ---------------- MAP PROCESS ---------------- #
+map_process = None
+
+def run_webview():
+    import webview
+    webview.create_window("IP Map", os.path.abspath(MAP_FILE), width=700, height=500, resizable=True)
+    webview.start(debug=False)
+
+def open_map_window(lat=0, lon=0, ip="N/A"):
+    global map_process
+    generate_map(lat, lon, ip)
+    if map_process is None or not map_process.is_alive():
+        map_process = Process(target=run_webview)
+        map_process.start()
+
+# ---------------- IP LOOKUP ---------------- #
 def lookup_ip_thread():
     ip = ip_entry.get().strip()
     if not ip:
@@ -52,15 +71,16 @@ def lookup_ip_thread():
         response = reader.city(ip)
         reader.close()
 
+        # Animate updates for all fields
         for var, val in zip(
             [country_var, region_var, city_var, postal_var, latitude_var, longitude_var],
             [response.country.name, response.subdivisions.most_specific.name, response.city.name,
              response.postal.code, str(response.location.latitude), str(response.location.longitude)]
         ):
-            threading.Thread(target=animate_field_update, args=(var, val or "N/A"), daemon=True).start()
+            animate_field_update(var, val or "N/A")
 
-        # Update map HTML for the new IP
-        generate_map(response.location.latitude, response.location.longitude, ip)
+        # Refresh map
+        open_map_window(response.location.latitude, response.location.longitude, ip)
 
     except Exception as e:
         messagebox.showerror("Error", f"Failed to lookup IP:\n{e}")
@@ -70,48 +90,49 @@ def lookup_ip_thread():
 def lookup_ip():
     threading.Thread(target=lookup_ip_thread, daemon=True).start()
 
+# ---------------- DETECT PUBLIC IP ---------------- #
 def detect_my_ip():
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-    ip_entry.delete(0, "end")
-    ip_entry.insert(0, local_ip)
-    lookup_ip()
+    loading_label.configure(text="Detecting public IP...")
+    
+    def detect_thread():
+        try:
+            # Try public IP
+            try:
+                ip = requests.get("https://api.ipify.org", timeout=5).text.strip()
+            except requests.RequestException:
+                # fallback to local network IP
+                hostname = socket.gethostname()
+                ip = socket.gethostbyname(hostname)
 
+            # Update entry safely
+            def update_entry():
+                ip_entry.delete(0, "end")
+                ip_entry.insert(0, ip)
+            root.after(0, update_entry)
+
+            # Lookup IP info and update GUI + map
+            lookup_ip_thread()
+
+        finally:
+            root.after(0, lambda: loading_label.configure(text=""))
+
+    threading.Thread(target=detect_thread, daemon=True).start()
+
+# ---------------- THEME ---------------- #
 def toggle_theme():
     current = ctk.get_appearance_mode()
     ctk.set_appearance_mode("Light" if current=="Dark" else "Dark")
-
-# ---------------- MAP PROCESS ---------------- #
-def run_webview():
-    """Runs PyWebview in a separate process"""
-    import webview
-    webview.create_window("IP Map", os.path.abspath(MAP_FILE), width=700, height=500, resizable=True)
-    webview.start(debug=False)
-
-map_process = None
-
-def open_map_window():
-    global map_process
-    if not os.path.exists(MAP_FILE):
-        generate_map(0, 0, "N/A")
-
-    # Start new PyWebview process if not running
-    if map_process is None or not map_process.is_alive():
-        map_process = Process(target=run_webview)
-        map_process.start()
-    else:
-        # Just overwrite MAP_FILE; the PyWebview process can be restarted manually if needed
-        generate_map(0, 0, "N/A")  # optional: refresh map
 
 # ---------------- GUI ---------------- #
 root = ctk.CTk()
 root.title("Modern AI IP Tracker")
 root.geometry("900x850")
 
+# ---------- Main Frame ---------- #
 main_frame = ctk.CTkFrame(root, corner_radius=15)
 main_frame.pack(fill="both", expand=True, padx=20, pady=20)
 
-# ----- Top bar: IP + buttons ----- #
+# ---------- Top Bar ---------- #
 top_bar1 = ctk.CTkFrame(main_frame, corner_radius=10)
 top_bar1.pack(fill="x", pady=(10,5), padx=10)
 
@@ -134,11 +155,11 @@ theme_btn.pack(side="left", padx=5)
 map_btn = ctk.CTkButton(top_bar2, text="Open Map", command=open_map_window, width=120, fg_color="#1f6aa5", hover_color="#3a86ff")
 map_btn.pack(side="left", padx=5)
 
-# ----- Loading label ----- #
+# ---------- Loading Label ---------- #
 loading_label = ctk.CTkLabel(main_frame, text="", text_color="orange")
 loading_label.pack(pady=5)
 
-# ----- Info panel ----- #
+# ---------- Info Panel ---------- #
 info_panel = ctk.CTkFrame(main_frame, corner_radius=15, fg_color="#1f1f2e", border_width=1, border_color="#3a86ff")
 info_panel.pack(fill="x", padx=20, pady=15)
 
@@ -164,8 +185,18 @@ for label, var in fields:
     ctk.CTkLabel(row, text=f"{label}: ", width=120, anchor="w").pack(side="left")
     ctk.CTkEntry(row, textvariable=var, state="readonly").pack(side="left", fill="x", expand=True)
 
-# ----- Footer ----- #
+# ---------- Footer ---------- #
 footer_label = ctk.CTkLabel(main_frame, text="Modern AI Offline IP Tracker", text_color="#3a86ff")
 footer_label.pack(pady=10)
 
+# ---------- Handle Close ---------- #
+def on_close():
+    global map_process
+    if map_process and map_process.is_alive():
+        map_process.terminate()
+    root.destroy()
+
+root.protocol("WM_DELETE_WINDOW", on_close)
+
+# ---------- Start GUI ---------- #
 root.mainloop()
